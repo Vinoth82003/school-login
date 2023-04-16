b9d4c5ad32076bc324a54d693c084ebdcf0ec023
Vinoth <118555830+Vinoth82003@users.noreply.github.com>
2023-04-17 00:11:53 +0530

    Update student.js

--- js/student.js
+++ js/student.js
@@ -1,6 +1,6 @@
 function printData()
 {
-   var divToPrint=document.getElementById("printTable");
+   var divToPrint=document.getElementById("marks");
    newWin= window.open("");
    newWin.document.write(divToPrint.outerHTML);
    newWin.print();
